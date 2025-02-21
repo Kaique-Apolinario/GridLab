7f3e3333dfe5391c3bc06914eccb7234574b3e8d
Kaique Apolinario <kaiqueapol@outlook.com>
2025-02-21 11:43:50 -0300

    Project creation and Apache POI dependency installation

---
+++ Back-end/sheet-manager/src/main/java/com/kaiqueapol/sheet_manager/SheetManagerApplication.java
@@ -0,0 +1,13 @@
+package com.kaiqueapol.sheet_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SheetManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SheetManagerApplication.class, args);
+	}
+
+}

79ce0bde42a40acae32cf8a965b965112b127658
unknown <kaiqueapol@outlook.com>
2025-04-05 16:21:13 -0300

    Done sheet extension validation

--- Back-end/sheetmanager/src/main/java/com/kaiqueapol/sheetmanager/controllers/SheetController.java
+++ Back-end/sheetmanager/src/main/java/com/kaiqueapol/sheetmanager/controllers/SheetController.java
@@ -1,6 +1,9 @@
 package com.kaiqueapol.sheetmanager.controllers;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.kaiqueapol.sheetmanager.services.SheetService;
 
@@ -12,4 +15,8 @@ public class SheetController {
 		this.sheetService = sheetService;
 	}
 
+	public ResponseEntity<String> uploadSheet(@RequestPart("file") MultipartFile file) throws Exception {
+		sheetService.divideSheets(file);
+		return ResponseEntity.ok("File updated sucessfully!");
+	}
 }
--- Back-end/sheetmanager/src/main/java/com/kaiqueapol/sheetmanager/services/SheetService.java
+++ Back-end/sheetmanager/src/main/java/com/kaiqueapol/sheetmanager/services/SheetService.java
@@ -1,8 +1,18 @@
 package com.kaiqueapol.sheetmanager.services;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
 public class SheetService {
 
+	public void divideSheets(MultipartFile file) throws Exception {
+		validateSheet(file);
+	}
+
+	public void validateSheet(MultipartFile file) throws Exception {
+		if (file.getName().endsWith(".xlsx") || (file.getName().endsWith(".xls"))) {
+			throw new Exception();
+		}
+	}
 }

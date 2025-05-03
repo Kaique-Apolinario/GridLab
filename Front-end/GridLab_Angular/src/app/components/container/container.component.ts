81fe16e06622f28505e1e1709f2617e9e2bcdfca
unknown <kaiqueapol@outlook.com>
2025-05-03 09:34:46 -0300

    Done cutter and merger toggle for smaller screens

--- Front-end/GridLab_Angular/src/app/components/container/container.component.ts
+++ Front-end/GridLab_Angular/src/app/components/container/container.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Signal, signal } from '@angular/core';
 import { UploadButtonComponent } from "../upload-button/upload-button.component";
 import { MergerComponent } from "../merger/merger.component";
 import { CommonModule } from '@angular/common';
@@ -10,6 +10,18 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './container.component.html',
   styleUrl: './container.component.scss'
 })
-export class ContainerComponent {
-  breakOrMerge: boolean = true;
+export class ContainerComponent implements OnInit{
+  screenWidth = signal(0);
+  fullOrPartialDisplay: string = "";
+  breakOrMerge: boolean = false;
+  
+  ngOnInit(): void {
+    if (typeof window !== 'undefined') {
+      this.screenWidth.set(window.innerWidth);
+      window.addEventListener("resize", () => {
+        this.screenWidth.set(window.innerWidth);
+      })
+    }
+  }
+
 }
